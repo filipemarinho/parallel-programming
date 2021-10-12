d0f4346833a4cabd36bb084e69e84eeec1418427
Filipe Marinho <filipemarinho@usp.br>
2021-10-12 16:38:32 -0300

    better file reading with fstream and checking filename

--- trabalho1/trab1.cpp
+++ trabalho1/trab1.cpp
@@ -10,30 +10,52 @@ int main(int argc, char *argv[])
 {
     string filename = argv[1];
 
-    if(filename.substr(filename.find_last_of(".") + 1) == "net") {
-    std::cout << "Nome do arquivo: " << filename << std::endl;
-    } else {
-        std::cout << "No..." << std::endl;
-        return 0;
-    }
-
-    // Read Line
-    string line;
-    int number;
-    int number1;
+    int vertA, vertB;
     // Read from the text file
     ifstream input_file(filename);
-    if (!input_file.is_open()) {
-        cerr << "Could not open the file - '"
+    if (!input_file.is_open() || filename.substr(filename.find_last_of(".") + 1) != "net") {
+        cerr << "Could not open the file, check extension. Filename - '"
              << filename << "'" << endl;
         return EXIT_FAILURE;
     }
 
-    while (input_file >> number >> number1) {
-        cout << number << "; "<< number1 << "; " << endl;
+    int n_vertices, n_arestas;
+    input_file >> n_vertices >> n_arestas;
+    cout << "Reading graph with " << n_vertices << " vertices and " << n_arestas << " arestas." << endl;
+
+    for (int i = 0; i < n_vertices; i++){
+        int vert_a, vert_b;
+        input_file >> vert_a >> vert_b;
+        cout << "Aresta: " << vert_a << "," << vert_b << endl;
     }
-    cout << endl;
+
     input_file.close();
 
     return 0;
 }
+
+
+/* 
+    // Number of vertices
+    getline(file, line);
+    int n_v = std::stoi(line);
+
+    //Number of arestas
+    getline(file, line);
+    int n_a = std::stoi(line);
+
+    cout << "Numero de vertices: " << n_v << ", Numero de arestas: " << n_a << endl;
+
+    //Identificadores de arestas
+    for (int i = 0; i < n_v; i++)
+    {
+        getline(file, line);
+        int vert_a;
+        sscanf(line.c_str(), "%d", &vert_a);
+        int vert_b;
+        sscanf(line.c_str(), "%d", &vert_b);
+        cout << "linha " << i << ": " << vert_a << "," << vert_b << endl; //Fails with numbers > 9, 
+    }
+
+    // Close the file
+    file.close(); */
