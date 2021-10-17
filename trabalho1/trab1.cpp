093938411c69d98324bb86dbda39f660c30ec84a
Filipe Marinho <filipemarinho@usp.br>
2021-10-17 18:41:08 -0300

    implemented adj list representation

--- trabalho1/trab1.cpp
+++ trabalho1/trab1.cpp
@@ -6,30 +6,61 @@
 
 using namespace std;
 
+void listAdj(vector<int> vert0, vector<int> vert1)
+{
+    vector<vector<int>> adjList(vert0.size());
+
+    for (int i = 0; i < vert0.size(); i++)
+    {
+        cout << "Linha: " << i << ", Aresta: " << vert0[i] << "," << vert1[i] << endl;
+        adjList[vert0[i]].push_back(vert1[i]);
+        adjList[vert1[i]].push_back(vert0[i]);
+    }
+
+
+    for (int i = 0; i < adjList.size(); i++)
+    {
+        cout << "Adj List of element " << i << endl;
+        for (int j = 0; j < adjList[i].size(); j++)
+        {
+            cout << adjList[i][j] << ", ";
+        }
+        cout << endl;
+    }
+
+    return;
+}
+
 int main(int argc, char *argv[])
 {
     string filename = argv[1];
 
-    int vertA, vertB;
     // Read from the text file
-    ifstream input_file(filename);
-    if (!input_file.is_open() || filename.substr(filename.find_last_of(".") + 1) != "net") {
+    ifstream inputFile(filename);
+    if (!inputFile.is_open() || filename.substr(filename.find_last_of(".") + 1) != "net")
+    {
         cerr << "Could not open the file, check extension. Filename - '"
              << filename << "'" << endl;
         return EXIT_FAILURE;
     }
 
-    int n_vertices, n_arestas;
-    input_file >> n_vertices >> n_arestas;
-    cout << "Reading graph with " << n_vertices << " vertices and " << n_arestas << " arestas." << endl;
+    int nVert, nArestas;
+    inputFile >> nVert >> nArestas;
+    cout << "Reading graph with " << nVert << " vertices and " << nArestas << " arestas." << endl;
 
-    for (int i = 0; i < n_vertices; i++){
-        int vert_a, vert_b;
-        input_file >> vert_a >> vert_b;
-        cout << "Aresta: " << vert_a << "," << vert_b << endl;
+    vector<int> vertA(nVert);
+    vector<int> vertB(nVert);
+
+    for (int i = 0; i < nVert; i++)
+    { //usar int i = 0; i < vect.size(); i++ se quiser ter certeza que o elemento está no vetor
+        inputFile >> vertA[i] >> vertB[i];
+        //cout << "Aresta: " << vertA[i] << "," << vertB[i] << endl;
     }
+    cout << "File read!" << endl;
 
-    input_file.close();
+    listAdj(vertA, vertB);
+
+    inputFile.close();
 
     return 0;
 }
