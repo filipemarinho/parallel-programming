11fb513039dcabad4948601970441a0a50969f73
Filipe Marinho <filipemarinho@usp.br>
2021-10-24 15:23:16 -0300

    lot of things

--- trabalho1/trab1.cpp
+++ trabalho1/trab1.cpp
@@ -5,30 +5,30 @@
 #include <string>
 #include <algorithm>
 #include <iomanip>
-// #include <time>
+#include <time.h>
 
 typedef std::vector<std::vector<int>> matrix_t;
 enum Error { SUCCESS, BAD_ARGUMENT, BAD_FILE };
 using namespace std;    
 class Graph {
     public:
-    // Graph();
     
     int nVertex, nEdges;
     vector<int> vertA, vertB;
     matrix_t adjList;
+    vector<int> degrees;
 
     //Return the adj list of a graph based on the vertex list
-    matrix_t getAdjList();
+    void getAdjList();
 
     // Read the input file and set graph
     void read(string filename);
 
     //Return the graph degree for each node
-    vector<int> getGraphDegree();
+    void getGraphDegree();
 
     //Return the graph rich club coefficient for a degree k 
-    int getRichClubCoef();
+    void getRichClubCoef();
 
     //Print Matrix to cout
     void printMatrix(matrix_t matrix);
@@ -52,14 +52,14 @@ void Graph::read(string filename){
     inputFile >> this->nVertex >> this->nEdges;
     cout << "Reading graph with " << this->nVertex << " vertices and " << this->nEdges << " arestas." << endl;
 
-    this->vertA.reserve(this->nEdges);
-    this->vertB.reserve(this->nEdges);
+    this->vertA.resize(this->nEdges, 0);
+    this->vertB.resize(this->nEdges,0);
 
     //Lê todas as arestas do arquivo
     for (int i = 0; i < this->nEdges; i++)
     {
         inputFile >> this->vertA[i] >> this->vertB[i];
-        // cout << "Aresta: " << vertA[i] << "," << vertB[i] << endl;
+        // cout << "Aresta: " << this->vertA[i] << "," << this->vertB[i] << endl;
     }
     cout << "File read!" << endl
          << "\n";
@@ -71,14 +71,19 @@ void Graph::read(string filename){
 
 int main(int argc, char *argv[])
 {
+    cout << std::setprecision(5) << std::fixed;
     string filename = argv[1];
 
-    Graph g1();
+    Graph g1;
 
     g1.read(filename);
 
     time_t start,end;
     time (&start);
+
+    g1.getAdjList();
+    g1.getGraphDegree();
+    g1.getRichClubCoef();
 
     time (&end);
     double dif = difftime (end,start);
@@ -88,53 +93,50 @@ int main(int argc, char *argv[])
 }
 
 
-matrix_t Graph::getAdjList()
+void Graph::getAdjList()
 {
-    matrix_t adjList(this->nEdges);
-
+    matrix_t adjList_(this->nEdges);
     for (int i = 0; i < this->vertA.size(); i++)
     {
-        //cout << "Linha: " << i << ", Aresta: " << this->vertA[i] << "," << this->vertB[i] << endl;
-        adjList[this->vertA[i]].push_back(this->vertB[i]);
-        adjList[this->vertB[i]].push_back(this->vertA[i]);
-    }
-
-    //cout << "List Adj Result" << endl;
-    //printMatrix(adjList);
-    return adjList;
+        // cout << "Linha: " << i << ", Aresta: " << this->vertA[i] << "," << this->vertB[i] << endl;
+        adjList_[this->vertA[i]].push_back(this->vertB[i]);
+        adjList_[this->vertB[i]].push_back(this->vertA[i]);
+    }   
+    this->adjList = adjList_;
+    // cout << "List Adj Result" << endl;
+    this->printMatrix(this->adjList);
+    return;
 }
 
 
-vector<int> Graph::getGraphDegree(){
+void Graph::getGraphDegree(){
 
-    vector<int> nodesDegree(this->adjList.size(), 0);
+    degrees.resize(this->adjList.size(), 0);
 
     // cout << "Graph Degree" << endl;
     for (auto i = 0; i < this->adjList.size(); i++){
         // cout << "Degree of " << i << ": " << this->adjList[i].size() << endl;
-        nodesDegree[i] = this->adjList[i].size();
+        degrees[i] = this->adjList[i].size();
     }
 
-    return nodesDegree;
+    return;
 }
 
-int Graph::getRichClubCoef(){
-    cout << std::setprecision(5) << std::fixed;
-
-    vector<int> degrees = getGraphDegree();
-    for (int k = 0; k< *max_element(degrees.begin(), degrees.end()); k++){
-    cout << "k = " << k << endl;
+void Graph::getRichClubCoef(){
+    
+    for (int k = 0; k< *max_element(this->degrees.begin(), this->degrees.end()); k++){
+    // cout << "k = " << k << endl;
     //Guarda o conjunto de arestas conectadas ao nó que pertence a R(k)
     vector<int> R_k;
-    for(int i = 0; i < degrees.size(); i++ ){
-        if (degrees[i]>k) {
+    for(int i = 0; i < this->degrees.size(); i++ ){
+        if (this->degrees[i]>k) {
             R_k.push_back(i);
         }
     }
 
     int nk =  R_k.size();
 
-    cout << "Graph Rich Club Coefficient for k = " << k << ", n_k = " << nk << endl;
+    // cout << "Graph Rich Club Coefficient for k = " << k << ", n_k = " << nk << endl;
 
     float rk = 0.;
 
@@ -149,7 +151,7 @@ int Graph::getRichClubCoef(){
     cout << "r(" << k <<") = " << rk << endl;
     
     }
-    return 0;
+    return ;
 }
 
 bool Graph::hasElement(vector<int> v, int element){
