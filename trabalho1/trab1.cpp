b87ee9dc45c7ab6a02267d29cecf9256c5c1bed1
Filipe Marinho <filipemarinho@usp.br>
2022-01-11 19:16:30 -0300

    exterminating evil classes

--- trabalho1/trab1.cpp
+++ trabalho1/trab1.cpp
@@ -1,4 +1,4 @@
-//g++ -o trab1.o trab1.cpp && ./trab1.o graph.net && diff -w rich-club-results/ rich-club-expected/
+//g++ -o trab1.o trab1.cpp && ./trab1.o rich-club-results/small-1-001.net && diff -w rich-club-results/medium-1-001.rcb rich-club-expected/medium-1-001.rcb 
 //Testar com flags adicionais: -g -std=c++17 -pedantic -Wall -Wextra -Werror -Wshadow -Wconversion -Wunreachable-code
 #include <iostream>
 #include <fstream>
@@ -19,31 +19,18 @@ enum Error
 };
 
 using namespace std;
-class Graph
-{
-    public:
-        int nVertex, nEdges;
-        vector<int> vertA, vertB;
-        matrix_t adjList;
-        vector<int> degrees;
-        int maxDegree = 0;
-        vector<float> rks;
 
-        // Lê o arquivo de entrada que contém o tamanho e as arestas do grafo
-        void read(string filename);
+// Lê o arquivo de entrada que contém o tamanho e as arestas do grafo
+void read(string filename, int &nVertex, int &nEdges, vector<int> &vertA, vector<int> &vertB);
+// Computa a lista de adj do grafo
 
-        // Computa a lista de adj do grafo
-        void getAdjList();
+// Computa o grau de cada nó
 
-        // Computa o grau de cada nó
-        void getGraphDegree();
+// Computa o coef. de clube dos ricos do grafo para o grau 0 até o grau máximo - 1
 
-        // Computa o coef. de clube dos ricos do grafo para o grau 0 até o grau máximo - 1
-        void getRichClubCoef();
 
-        // Armazena os resultados no arquivo de saida com a extensão .rcb
-        void printResult(string filenameOutput);
-};
+// Armazena os resultados no arquivo de saida com a extensão .rcb
+void printResult(string filenameOutput, vector<float> rks);
 
 int main(int argc, char *argv[])
 {
@@ -52,27 +39,86 @@ int main(int argc, char *argv[])
     cout << std::setprecision(5) << std::fixed;
 
     //Lê e inicializa o grafo
-    Graph g1;
-    g1.read(filename);
+    int nVertex, nEdges;
+    vector<int> vertA, vertB;
 
+    read(filename, nVertex, nEdges, vertA, vertB);
+    
     //Cronometrando tempo de execução
     auto t1 = std::chrono::high_resolution_clock::now();
 
+    /* Para implementação dos grafos foi escolhida a lista de adj por lidar bem com grafos esparsos
+    ao contrário da matriz de adj que requer NxN elementos para representação mesmo que existam poucas ligações
+    e por facilitar o acesso aos vizinhos do vértice.
+    */
+
+    matrix_t adjList(nVertex);
+
+    // Para cada vertice atualiza a lista de adj do nó correspondente
+    for (size_t i = 0; i < vertA.size(); i++)
+    {
+        adjList[vertA[i]].emplace_back(vertB[i]);
+        adjList[vertB[i]].emplace_back(vertA[i]);
+    }
+
+    vector<int> degrees(nVertex, 0);
+    int maxDegree = 0;
+
+    // Calcula o grau de cada nó e obtem o grau máximo
+    for (auto i = 0; i < adjList.size(); i++)
+    {
+        degrees[i] = (int)adjList[i].size();
+        if (degrees[i] > maxDegree) /*Obter o grau máximo aqui reduz um loop na lista de graus*/
+            maxDegree = degrees[i];
+    }
+    
+    vector<float> rks;
+
     //Calcula o coeficiente
-    g1.getAdjList();
-    g1.getGraphDegree();
-    g1.getRichClubCoef();
+    for (int k = 0; k < maxDegree; k++) // Para cada k até k_max -1 calcula o coef. do clube dos ricos
+    {
+        vector<int> R_k; // Armazena o lista de vertices do clube dos ricos
+        float rk = 0.;   // Coeficiente de clube dos ricos
+
+        // Acha os nós com grau > k e conta os vizinhos com grau > k
+        for (size_t i = 0; i < degrees.size(); i++)
+        {
+            if (degrees[i] > k)
+            {
+                // Armazena o vértice no clube dos ricos 
+                R_k.emplace_back(i);
+
+                /* Procurar iterativamente na lista de adj ao incluir um vértice foi a melhor alternativa 
+                encontrada para calcular o valor do somatorio do coef. rk,
+                pois principalmente para k pequeno o custo de percorrer Rk é muito maior que percorrer a lista de adj do vértice. */
+                // Procura na lista de adj por conexões que tenham grau maior que k
+                std::for_each(adjList[i].begin(), adjList[i].end(), [&](auto &item) -> void
+                              {
+                                  if (degrees[item] > k)
+                                      rk += 1;
+                              });
+            }
+        }
+
+        int nk = (int) R_k.size();
+        if (nk > 1)
+            rk /= (float)(nk * (nk - 1.));
+        else
+            rk = 1;
+
+        rks.emplace_back(rk);
+    }
 
     auto t2 = std::chrono::high_resolution_clock::now();
     auto dif = std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
     cout << "Elasped time " << dif << " seconds." << endl;
 
-    g1.printResult(filename);
+    printResult(filename, rks);
 
     return 0;
 }
 
-void Graph::read(string filename)
+void read(string filename, int &nVertex, int &nEdges, vector<int> &vertA, vector<int> &vertB)
 {
     // Abre o arquivo para leitura
     ifstream inputFile(filename);
@@ -86,107 +132,29 @@ void Graph::read(string filename)
     }
 
     // Lê o numero de Vertices e Arestas
-    inputFile >> this->nVertex >> this->nEdges;
+    inputFile >> nVertex >> nEdges;
 
     /* Reservar evita que o vector tenha que ser realocado dinâmicamente diversas vezes */
     // Reserva espaço para alocar todas as arestas do grafo
-    this->vertA.reserve(this->nEdges);
-    this->vertB.reserve(this->nEdges);
+    vertA.reserve(nEdges);
+    vertB.reserve(nEdges);
 
     //Le as arestas
-    for (int i = 0; i < this->nEdges; i++)
+    for (int i = 0; i < nEdges; i++)
     {
         int tempA, tempB;
         inputFile >> tempA >> tempB;
-        this->vertA.emplace_back(tempA);
-        this->vertB.emplace_back(tempB);
+        vertA.emplace_back(tempA);
+        vertB.emplace_back(tempB);
     }
 
     inputFile.close();
     return;
 }
 
-void Graph::getAdjList()
+
+void printResult(string filename, vector<float> rks)
 {
-    /* Para implementação dos grafos foi escolhida a lista de adj por lidar bem com grafos esparsos
-    ao contrário da matriz de adj que requer NxN elementos para representação mesmo que existam poucas ligações
-    e por facilitar o acesso aos vizinhos do vértice.
-
-    */
-    matrix_t adjList_(this->nVertex);
-
-    // Para cada vertice atualiza a lista de adj do nó correspondente
-    for (size_t i = 0; i < this->vertA.size(); i++)
-    {
-        adjList_[this->vertA[i]].emplace_back(this->vertB[i]);
-        adjList_[this->vertB[i]].emplace_back(this->vertA[i]);
-    }
-
-    this->adjList = adjList_;
-
-    return;
-}
-
-void Graph::getGraphDegree()
-{
-
-    degrees.resize(this->nVertex, 0);
-
-    // Calcula o grau de cada nó e obtem o grau máximo
-    for (auto i = 0; i < this->adjList.size(); i++)
-    {
-        degrees[i] = (int)this->adjList[i].size();
-
-        if (degrees[i] > this->maxDegree) /*Obter o grau máximo aqui reduz um loop na lista de graus*/
-            this->maxDegree = degrees[i];
-    }
-
-    return;
-}
-
-void Graph::getRichClubCoef()
-{
-    for (int k = 0; k < this->maxDegree; k++) // Para cada k até k_max -1 calcula o coef. do clube dos ricos
-    {
-        vector<int> R_k; // Armazena o lista de vertices do clube dos ricos
-        float rk = 0.;   // Coeficiente de clube dos ricos
-
-        // Acha os nós com grau > k e conta os vizinhos com grau > k
-        for (size_t i = 0; i < this->degrees.size(); i++)
-        {
-            if (this->degrees[i] > k)
-            {
-                // Armazena o vértice no clube dos ricos 
-                R_k.emplace_back(i);
-
-                /* Procurar iterativamente na lista de adj ao incluir um vértice foi a melhor alternativa 
-                encontrada para calcular o valor do somatorio do coef. rk,
-                pois principalmente para k pequeno o custo de percorrer Rk é muito maior que percorrer a lista de adj do vértice. */
-                // Procura na lista de adj por conexões que tenham grau maior que k
-                std::for_each(adjList[i].begin(), adjList[i].end(), [&](auto &item) -> void
-                              {
-                                  if (this->degrees[item] > k)
-                                      rk += 1;
-                              });
-            }
-        }
-
-        int nk = (int) R_k.size();
-
-        if (nk > 1)
-            rk /= (float)(nk * (nk - 1.));
-        else
-            rk = 1;
-
-        this->rks.emplace_back(rk);
-    }
-
-    return;
-}
-
-void Graph::printResult(string filename)
-{
-
     // Muda a extensão
     filename.resize(filename.size() - 3);
     filename += "rcb";
@@ -199,7 +167,7 @@ void Graph::printResult(string filename)
 
     outputFile << fixed << setprecision(5);
 
-    for (size_t i = 0; i < this->rks.size(); i++)
+    for (size_t i = 0; i < rks.size(); i++)
     {
         outputFile << rks[i] << endl;
     }
