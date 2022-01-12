44fe1f8125a2ac4b01a1f52c2b0dfad84f008770
Filipe Marinho <filipemarinho@usp.br>
2022-01-11 23:53:17 -0300

    wrong values, but working

--- MPI/trab3.cpp
+++ MPI/trab3.cpp
@@ -1,16 +1,16 @@
-// mpic++ -g -std=c++17 -O2 -Wpedantic -Wall -Wextra -o trab3.o trab3.cpp && mpirun -oversubscribe -np 5 ./trab3.o ../../../rich-club-results/huge-1-000.net
+//g++ -o trab1.o trab1.cpp && ./trab1.o rich-club-results/small-1-001.net && diff -w rich-club-results/medium-1-001.rcb rich-club-expected/medium-1-001.rcb 
+//Testar com flags adicionais: -g -std=c++17 -pedantic -Wall -Wextra -Werror -Wshadow -Wconversion -Wunreachable-code
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
 #include <vector>
 #include <string>
 #include <algorithm>
 #include <iomanip>
 #include <chrono>
 #include <mpi.h>
+#include <stdio.h>
 
 typedef std::vector<std::vector<int>> matrix_t;
-using namespace std;
 
 enum Error
 {
@@ -19,92 +19,145 @@ enum Error
     BAD_FILE,
     BAD_RESULT
 };
-class Graph
-{
-    public:
-        int nVertex, nEdges;
-        vector<int> vertA, vertB;
-        matrix_t adjList;
-        vector<int> degrees;
-        int maxDegree = 0;
-        vector<float> rks;
 
-        // Lê o arquivo de entrada que contém o tamanho e as arestas do grafo
-        void read(string filename);
 
-        // Computa a lista de adj do grafo
-        void getAdjList();
+using namespace std;
 
-        // Computa o grau de cada nó
-        void getGraphDegree();
-
-        // Computa o coef. de clube dos ricos do grafo para o grau 0 até o grau máximo - 1
-        void getRichClubCoef();
-
-        // Armazena os resultados no arquivo de saida com a extensão .rcb
-        void printResult(string filenameOutput);
+struct adj{
+    int at = -1;
+    int size = 0;
+    vector<int> elements;
 };
+// Lê o arquivo de entrada que contém o tamanho e as arestas do grafo
+void read(string filename, int &nVertex, int &nEdges, vector<int> &vertA, vector<int> &vertB);
+
+// Armazena os resultados no arquivo de saida com a extensão .rcb
+void printResult(string filenameOutput, vector<float> rks);
 
 int main(int argc, char *argv[])
 {
-    MPI_Init(&argc, &argv); // antes de usar agrc e argv, pode alterar esses argumentos para utilização
+    MPI_Init(&argc, &argv);
 
     int nprocs, rank;
 
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-
     printf("Process %d of %d is running.\n", rank, nprocs);
-
-    std::chrono::high_resolution_clock::time_point t1,t2; 
-    cout << std::setprecision(5) << std::fixed;
-
-
+    
     string filename = argv[1];
-    Graph g1;
 
-    if (rank == 0){
-    g1.read(filename);
-    }
+    //Lê e inicializa o grafo
+    int nVertex, nEdges;
+    vector<int> vertA, vertB;
 
-    MPI_Bcast(&g1.nVertex, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    printf("Process %d of %d: has nVertex = %d.\n", rank, nprocs, g1.nVertex);
+    if (rank == 0) read(filename, nVertex, nEdges, vertA, vertB);
+    
+    MPI_Bcast(&nVertex, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("Process %d of %d: has nVertex = %d.\n", rank, nprocs, nVertex);
 
-    MPI_Bcast(&g1.nEdges, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    printf("Process %d of %d: has nEdges = %d.\n", rank, nprocs, g1.nEdges);
+    MPI_Bcast(&nEdges, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("Process %d of %d: has nEdges = %d.\n", rank, nprocs, nEdges);
 
     if (rank !=0){
-    g1.vertA.reserve(g1.nEdges);
-    g1.vertB.reserve(g1.nEdges);
+    vertA.reserve(nEdges);
+    vertB.reserve(nEdges);
     }
-    MPI_Bcast(&g1.vertA[0], g1.nEdges, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&g1.vertB[0], g1.nEdges, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Barrier(MPI_COMM_WORLD);
-    printf("Process %d of %d: after vertA[0] = %d .\n", rank, nprocs, g1.vertA[0]);
-    printf("Process %d of %d: after vertB[0] = %d .\n", rank, nprocs, g1.vertB[0]);
+    MPI_Bcast(&vertA[0], nEdges, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&vertB[0], nEdges, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("Process %d of %d: after vertA[0] = %d .\n", rank, nprocs, vertA[nEdges-1]);
+    printf("Process %d of %d: after vertB[0] = %d .\n", rank, nprocs, vertB[nEdges-1]);
 
-    // //Cronometrando tempo de execução
-    t1 = std::chrono::high_resolution_clock::now();
+    //Cronometrando tempo de execução
+    auto t1 = std::chrono::high_resolution_clock::now();
 
-    g1.getAdjList();
-    g1.getGraphDegree();
-    // g1.getRichClubCoef();
+    /* Para implementação dos grafos foi escolhida a lista de adj por lidar bem com grafos esparsos
+    ao contrário da matriz de adj que requer NxN elementos para representação mesmo que existam poucas ligações
+    e por facilitar o acesso aos vizinhos do vértice.
+    */
+    // Obtendo a lista de adj do grafo
+    vector<vector<int>> adjList(nVertex);
+    // Para cada vertice atualiza a lista de adj do nó correspondente
+    for (int i = 0; i < nEdges; i++)
+    {
+        adjList[vertA[i]].emplace_back(vertB[i]);
+        adjList[vertB[i]].emplace_back(vertA[i]);
+        // printf("Process %d: has vertA[%d] = %d, vertB[%d] = %d \n", rank, i, vertA[i], i, vertB[i]);
+    }
+    // Obtendo o grau de cada nó
+    vector<int> degrees(nVertex, 0);
+    int maxDegree = 0;
+    // Calculo do grau de cada nó e o grau máximo
+    for (auto i = 0; i < nVertex; i++)
+    {
+        degrees[i] = (int) adjList[i].size();
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    if (rank == 0){
-    t2 = std::chrono::high_resolution_clock::now();
+        if (degrees[i] == 0) {
+            printf("Strange... \n Process %d of %d: has degrees[%d] = %d.\n", rank, nprocs, i, degrees[i]);
+        }
+        // printf("Process %d of %d: has maxDegree = %d, degrees[i] = %d, adjlist.size = %d .\n", rank, nprocs, maxDegree, degrees[i], adjList[i].size());
+        //Obter o grau máximo aqui reduz um loop na lista de graus
+        if (degrees[i] > maxDegree) 
+            maxDegree = degrees[i];
+    }
+    if (maxDegree = 0) {
+        printf("Bad max degree :C");
+        MPI_Finalize();
+        exit(BAD_ARGUMENT);
+    }
+    // Calculo do coef. de clube dos ricos do grafo para o grau 0 até o grau máximo - 1
+    vector<float> rks(maxDegree, 0.0); // Garante que o tamanho não seja alterado dinamicamente
+    // printf("Process %d of %d: has maxDegree = %d, rks.size = %d .\n", rank, nprocs, maxDegree, rks.size());
+    //Calcula o coeficiente
+    for (int k = 0; k < maxDegree; k++) // Para cada k até k_max -1 calcula o coef. do clube dos ricos
+    {
+        vector<int> R_k; // Armazena o lista de vertices do clube dos ricos
+        float rk = 0.;   // Coeficiente de clube dos ricos
+
+        // Acha os nós com grau > k e conta os vizinhos com grau > k
+        for (size_t i = 0; i < nVertex; i++)
+        {
+            if (degrees[i] > k)
+            {
+                // Armazena o vértice no clube dos ricos 
+                R_k.emplace_back(i);
+
+                /* Procurar iterativamente na lista de adj ao incluir um vértice foi a melhor alternativa 
+                encontrada para calcular o valor do somatorio do coef. rk,
+                pois principalmente para k pequeno o custo de percorrer Rk é muito maior que percorrer a lista de adj do vértice. */
+                // Procura na lista de adj por conexões que tenham grau maior que k
+                std::for_each(adjList[i].begin(), adjList[i].end(), [&](auto &item) -> void
+                              {
+                                  if (degrees[item] > k)
+                                      rk += 1;
+                              });
+            }
+        }
+
+        int nk = (int) R_k.size();
+        if (nk > 1)
+            rk /= (float)(nk * (nk - 1.));
+        else
+            rk = 1;
+
+        rks[k] = rk;
+        printf("Process %d of %d: has rk[%d] = %f .\n", rank, nprocs, k, rks[k]);
+    }
+    // printf("Process %d of %d: has rks.size = %d .\n", rank, nprocs, rks.size());
+
+    MPI_Barrier( MPI_COMM_WORLD);
+    // printf("Process %d of %d: after rk[0] = %f .\n", rank, nprocs, rks[0]);
+    // printf("Process %d of %d: after rk[1] = %f .\n", rank, nprocs, rks[1]);
+    auto t2 = std::chrono::high_resolution_clock::now();
     auto dif = std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
-    printf("Total elasped time: %d seconds\n", dif);
+    cout << "Elasped time " << dif << " seconds." << endl;
 
-    // g1.printResult(filename);
-    }
-    
+    if (rank ==0) printResult(filename, rks);
     MPI_Finalize();
     return SUCCESS;
 }
 
-void Graph::read(string filename)
+void read(string filename, int &nVertex, int &nEdges, vector<int> &vertA, vector<int> &vertB)
 {
     // Abre o arquivo para leitura
     ifstream inputFile(filename);
@@ -115,139 +168,47 @@ void Graph::read(string filename)
         cerr << "Could not open the file, check extension. Filename - '"
              << filename << "'" << endl;
         MPI_Finalize();
-        exit(BAD_FILE);
+        exit(BAD_ARGUMENT);
     }
 
     // Lê o numero de Vertices e Arestas
-    inputFile >> this->nVertex >> this->nEdges;
+    inputFile >> nVertex >> nEdges;
 
     /* Reservar evita que o vector tenha que ser realocado dinâmicamente diversas vezes */
     // Reserva espaço para alocar todas as arestas do grafo
-    this->vertA.reserve(this->nEdges);
-    this->vertB.reserve(this->nEdges);
+    vertA.reserve(nEdges);
+    vertB.reserve(nEdges);
 
     //Le as arestas
-    for (int i = 0; i < this->nEdges; i++)
+    for (int i = 0; i < nEdges; i++)
     {
         int tempA, tempB;
         inputFile >> tempA >> tempB;
-        this->vertA.emplace_back(tempA);
-        this->vertB.emplace_back(tempB);
+        vertA.emplace_back(tempA);
+        vertB.emplace_back(tempB);
     }
 
     inputFile.close();
-
-    printf("Read graph from %s \n with %d vertex and %d edges \n", filename.c_str(), this->nVertex, this->nEdges);
-
     return;
 }
 
-void Graph::getAdjList()
+
+void printResult(string filename, vector<float> rks)
 {
-    /* Para implementação dos grafos foi escolhida a lista de adj por lidar bem com grafos esparsos
-    ao contrário da matriz de adj que requer NxN elementos para representação mesmo que existam poucas ligações
-    e por facilitar o acesso aos vizinhos do vértice.
-    */
-
-    matrix_t adjList_(this->nVertex);
-
-    // Para cada vertice atualiza a lista de adj do nó correspondente
-    for (size_t i = 0; i < this->vertA.size(); i++)
-    {
-        adjList_[this->vertA[i]].emplace_back(this->vertB[i]);
-        adjList_[this->vertB[i]].emplace_back(this->vertA[i]);
-    }
-
-    this->adjList = adjList_;
-
-    return;
-}
-
-void Graph::getGraphDegree()
-{
-    this->degrees.resize(this->nVertex, 0);
-    // Calcula o grau de cada nó e obtem o grau máximo
-    for (auto i = 0; i < this->adjList.size(); i++)
-    {
-        this->degrees[i] = (int)this->adjList[i].size();
-       if (this->degrees[i] > this->maxDegree) /*Obter o grau máximo aqui reduz um loop na lista de graus*/
-           this->maxDegree = this->degrees[i];
-    }
-    return;
-}
-
-void Graph::getRichClubCoef()
-{
-
-    // int maxDegree = this->maxDegree;
-    vector<float> _rks(maxDegree, 0.0); // Garante que o tamanho não seja alterado dinamicamente
-    // vector<int> degrees = this->degrees;
-
-    // //  shared(degrees, adjList, _rks, maxDegree)
-    for (int k = 0; k < maxDegree; k++) // Para cada k até k_max-1 calcula o coef. do clube dos ricos
-    { 
-
-        float rk = 0.;   // Coeficiente de clube dos ricos
-        int nk = 0;
-        int i;
-
-        // Acha os nós com grau > k e conta os vizinhos com grau > k
-        //  shared(degrees,k) private(i) reduction(+:rk) reduction(+:nk)
-        for (i = 0; i < degrees.size(); i++)
-        {
-            if (degrees[i] > k)
-            {
-                
-                // Armazena o numero total de membros do clube dos ricos 
-                nk += 1;
-
-                /* Procurar iterativamente na lista de adj ao incluir um vértice foi a melhor alternativa 
-                encontrada para calcular o valor do somatorio do coef. rk, pois principalmente para k 
-                pequeno o custo de percorrer Rk é muito maior que percorrer a lista de adj do vértice. */
-                // Procura na lista de adj por conexões que tenham grau maior que k
-                std::for_each(adjList[i].begin(), adjList[i].end(), [&](auto &item) -> void
-                              {
-                                  if (degrees[item] > k)
-                                      // Calculo o somatório contando cada vizinho que também participa do clube
-                                      rk += 1;
-                              });
-            }
-        }
-
-        if (nk > 1){
-            rk /= (float)(nk * (nk - 1.));
-        }
-        else{
-            rk = 1;
-        }
-
-        _rks[k] = rk;
-    }
-    // float *data = NULL;
-    // if (rank==0) data = (float *) malloc(nprocs)*sizeof(float); 
-    // MPI_Gather(&rk, 2, MPI_FLOAT, &data, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-    // this->rks = _rks;
-    return;
-}
-
-void Graph::printResult(string filename)
-{
-
     // Muda a extensão
     filename.resize(filename.size() - 3);
     filename += "rcb";
     ofstream outputFile(filename, ios::trunc); //Cria ou sobrescreve o arquivo de resultados
-    cout << "Arquivo de saida: " << filename << endl;
     if (!outputFile.is_open())
     {
         cerr << "Could not open the output file: " << filename << endl;
         MPI_Finalize();
-        exit(BAD_ARGUMENT);
+        exit(BAD_FILE);
     }
 
     outputFile << fixed << setprecision(5);
 
-    for (size_t i = 0; i < this->rks.size(); i++)
+    for (size_t i = 0; i < rks.size(); i++)
     {
         outputFile << rks[i] << endl;
     }
