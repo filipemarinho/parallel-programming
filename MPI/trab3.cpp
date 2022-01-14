02a05d895b78850ef08b50fff9463e61b1ae3530
Filipe Marinho <filipemarinho@usp.br>
2022-01-14 10:19:33 -0300

    balancing k division, more elements for last ks

--- MPI/trab3.cpp
+++ MPI/trab3.cpp
@@ -1,5 +1,54 @@
 //g++ -o trab1.o trab1.cpp && ./trab1.o rich-club-results/small-1-001.net && diff -w rich-club-results/medium-1-001.rcb rich-club-expected/medium-1-001.rcb 
 //Testar com flags adicionais: -g -std=c++17 -pedantic -Wall -Wextra -Werror -Wshadow -Wconversion -Wunreachable-code
+/*
+np = 2
+real    9m27,862s
+user    17m11,299s
+sys     0m17,614s
+
+np = 4
+real    10m22,855s
+user    35m57,465s
+sys     0m34,143s
+
+np = 8
+real    15m26,972s
+user    70m16,303s
+sys     38m19,185s
+
+real    13m33,779s
+user    66m20,910s
+sys     33m29,364s
+
+real    14m5,722s
+user    67m35,047s
+sys     36m25,092s
+
+np = 16
+real    20m58,988s
+user    88m17,842s
+sys     66m57,755s
+
+without  Rk
+real    21m21,862s
+user    89m57,968s
+sys     68m25,752s
+
+-o2, np = 16
+real    8m31,697s
+user    35m47,770s
+sys     25m27,927s
+
+real    8m20,293s
+user    34m48,595s
+sys     24m54,379s
+-o2, np = 8
+real    4m55,619s
+user    22m26,317s
+sys     11m35,146s
+
+*/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -23,19 +72,24 @@ enum Error
     BAD_RESULT
 };
 
-
 using namespace std;
 
-struct adj{
-    int at = -1;
-    int size = 0;
-    vector<int> elements;
-};
 // Lê o arquivo de entrada que contém o tamanho e as arestas do grafo
 void read(string filename, int &nVertex, int &nEdges, vector<int> &vertA, vector<int> &vertB);
 
 // Armazena os resultados no arquivo de saida com a extensão .rcb
 void printResult(string filenameOutput, vector<float> rks);
+
+void compute_partition(int N, int p, vector<int> &count, vector<int> &offset) {
+  int q = N / p;
+  int r = N % p;
+  int curr_off = 0;
+  for (int i = 0; i < p; ++i) {
+    count[i] = i < r ? q : q+1+r;
+    offset[i] = curr_off;
+    curr_off += count[i];
+  }
+}
 
 int main(int argc, char *argv[])
 {
@@ -45,8 +99,6 @@ int main(int argc, char *argv[])
 
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    // printf("Process %d of %d is running.\n", rank, nprocs);
     
     string filename = argv[1];
 
@@ -55,12 +107,11 @@ int main(int argc, char *argv[])
     vector<int> vertA, vertB;
 
     if (rank == 0) read(filename, nVertex, nEdges, vertA, vertB);
-    
-    MPI_Bcast(&nVertex, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    // printf("Process %d of %d: has nVertex = %d.\n", rank, nprocs, nVertex);
+    int parallelThreshold = 1000;
 
+
+    MPI_Bcast(&nVertex, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&nEdges, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    // printf("Process %d of %d: has nEdges = %d.\n", rank, nprocs, nEdges);
 
     if (rank !=0){
     vertA.reserve(nEdges);
@@ -68,8 +119,6 @@ int main(int argc, char *argv[])
     }
     MPI_Bcast(&vertA[0], nEdges, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&vertB[0], nEdges, MPI_INT, 0, MPI_COMM_WORLD);
-    // printf("Process %d of %d: after vertA[0] = %d .\n", rank, nprocs, vertA[nEdges-1]);
-    // printf("Process %d of %d: after vertB[0] = %d .\n", rank, nprocs, vertB[nEdges-1]);
 
     //Cronometrando tempo de execução
     auto t1 = std::chrono::high_resolution_clock::now();
@@ -85,7 +134,6 @@ int main(int argc, char *argv[])
     {
         adjList[vertA[i]].emplace_back(vertB[i]);
         adjList[vertB[i]].emplace_back(vertA[i]);
-        // printf("Process %d: has vertA[%d] = %d, vertB[%d] = %d \n", rank, i, vertA[i], i, vertB[i]);
     }
     // Obtendo o grau de cada nó
     vector<int> degrees(nVertex, 0);
@@ -98,7 +146,6 @@ int main(int argc, char *argv[])
         if (degrees[i] == 0) {
             printf("Strange... \n Process %d of %d: has degrees[%d] = %d.\n", rank, nprocs, i, degrees[i]);
         }
-        // printf("Process %d of %d: has maxDegree = %d, degrees[i] = %d, adjlist.size = %d .\n", rank, nprocs, maxDegree, degrees[i], adjList[i].size());
         //Obter o grau máximo aqui reduz um loop na lista de graus
         if (degrees[i] > maxDegree) 
             maxDegree = degrees[i];
@@ -109,17 +156,37 @@ int main(int argc, char *argv[])
         exit(BAD_ARGUMENT);
     }
 
+    // vector<float> partial_rk_list;
+    // vector<int> counts(nprocs), displs(nprocs);
     vector<float> partial_rk_list;
-    vector<int> counts(nprocs), displs(nprocs);
-    //Divisão dos passos
-    int kmax = maxDegree;                       //kmax
-    int q = kmax / nprocs;                      // Quociente
-    int r = kmax % nprocs;                      // Resto
-    auto first_q = fill_n(begin(counts), r, q + 1); // Q + 1 no primeiro contador
-    fill(first_q, end(counts), q);                  // Q no resto
-    displs[0] = 0;
-    partial_sum(begin(counts), end(counts) - 1, begin(displs) + 1); // Preenche displs com o índice que cada proc deve iniciar
+    vector<int> count(nprocs), offset(nprocs);
+    int N = maxDegree;
+    compute_partition(N, nprocs, count, offset);
 
+
+    //Divisão dos elementos para os os processos
+    int kmax = maxDegree;                       //kmax = n_elem
+    // int q = kmax / (nprocs);                      // Quociente
+    // int r = kmax % (nprocs);                      // Resto
+    // auto first_q = fill_n(begin(counts), r, q + 1); // Q + 1 elementos no primeiro contador
+    // printf("rank = %d: ", rank);
+    // for (int x = 0; x < nprocs; x++){
+    //     printf("counts[%d] = %d ", x, counts[x]);
+    // }
+    // fill(first_q, end(counts), q);                  // Q elementos no resto
+    // for (int x = 0; x < nprocs; x++){
+    //     printf("counts_a[%d] = %d ", x, counts[x]);
+    // }
+    // printf("rank = %d: \n", rank);
+    // displs[0] = 0;
+    // partial_sum(begin(counts), end(counts) - 1, begin(displs) + 1); // Preenche displs com o índice que cada proc deve iniciar
+    // if (rank ==0 )printf("kmax = %d, q = %d , r = %d \n", kmax, q, r);
+    // for (int x = 0; x < nprocs; x++){
+    //     printf("displs[%d] = %d, ", x, displs[x]);
+    //     printf("counts [%d] = %d, ", x, counts[x]);
+    //     // printf("displs[%d] = k = %d, r = %d , q*x = %d\n", x, novo, rank, q*x);
+    // }
+    // printf("rank = %d: \n", rank);
     // Calculo do coef. de clube dos ricos do grafo para o grau 0 até o grau máximo - 1
     // vector<float> rks(maxDegree, 0.0); // Garante que o tamanho não seja alterado dinamicamente
     // printf("Process %d of %d: has rks = %d .\n", rank, nprocs, maxDegree);
@@ -127,11 +194,12 @@ int main(int argc, char *argv[])
     //Calcula o coeficiente
     // for (int k = 0; k < maxDegree; k++) // Para cada k até k_max -1 calcula o coef. do clube dos ricos
         //For levando em conta a divisão dos passos para o calculo do rcc
-    for (int k = displs[rank]; k < displs[rank] + counts[rank]; k++)
+    // for (int k = displs[rank]+ counts[rank]; k < displs[rank] ; k--)
+    for (int k = offset[rank]; k < offset[rank] + count[rank] ; k++)
     {
-        k = kmax - k;
-        vector<int> R_k; // Armazena o lista de vertices do clube dos ricos
+        // vector<int> R_k; // Armazena o lista de vertices do clube dos ricos
         float rk = 0.;   // Coeficiente de clube dos ricos
+        int nk = 0;
 
         // Acha os nós com grau > k e conta os vizinhos com grau > k
         for (size_t i = 0; i < nVertex; i++)
@@ -139,7 +207,8 @@ int main(int argc, char *argv[])
             if (degrees[i] > k)
             {
                 // Armazena o vértice no clube dos ricos 
-                R_k.emplace_back(i);
+                // R_k.emplace_back(i);
+                nk += 1;
 
                 /* Procurar iterativamente na lista de adj ao incluir um vértice foi a melhor alternativa 
                 encontrada para calcular o valor do somatorio do coef. rk,
@@ -153,28 +222,29 @@ int main(int argc, char *argv[])
             }
         }
 
-        int nk = (int) R_k.size();
-        if (nk > 1)
+        // int nk = (int) R_k.size();
+        // if (nk > 1)
+        //     rk /= (float)(nk * (nk - 1.));
+        // else
+        //     rk = 1;
+        if (nk > 1){
             rk /= (float)(nk * (nk - 1.));
-        else
+        }
+        else{
             rk = 1;
+        }
 
         // rks[k] = rk;
         partial_rk_list.push_back(rk);
-        // printf("Process %d of %d: has rk[%d] = %f .\n", rank, nprocs, k, rks[k]);
         printf("Process %d of %d: has k = %d, rk = %.5f, kmax = %d.\n", rank, nprocs, k, rk, kmax);
     }
-    // printf("Process %d of %d: has rks = %d .\n", rank, nprocs, rks[0]);
 
     MPI_Barrier( MPI_COMM_WORLD);
 
     // Transferencia dos dados para o rank 0
     vector<float> rks = vector<float>(kmax); // Variavel real
-    MPI_Gatherv(&partial_rk_list[0], counts[rank], MPI_FLOAT,
-                &rks[0], &counts[0], &displs[0], MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    // printf("Process %d of %d: after rk[0] = %f .\n", rank, nprocs, rks[0]);
-    // printf("Process %d of %d: after rk[1] = %f .\n", rank, nprocs, rks[1]);
+    // MPI_Gatherv(&partial_rk_list[0], counts[rank], MPI_FLOAT, &rks[0], &counts[0], &displs[0], MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(&partial_rk_list[0], count[rank], MPI_FLOAT, &rks[0], &count[0], &offset[0], MPI_FLOAT, 0, MPI_COMM_WORLD);
 
 
     if (rank == 0) {
